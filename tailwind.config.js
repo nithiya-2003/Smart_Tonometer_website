6843074eb0436e2134c06dde3717c6a4b20bff67
NITHIYASHREE R <166826172+nithiya-2003@users.noreply.github.com>
2025-02-08 17:23:44 +0000

    deletd js files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-    "./templates/**/*.{html,js}",
-    "./static/**/*.{html,js}",
-];
-export const theme = {
-    extend: {},
-};
-export const plugins = [];
