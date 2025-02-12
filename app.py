bbc75fa9255fd5afb9429133e5ea751f74551b26
NITHIYASHREE R <166826172+nithiya-2003@users.noreply.github.com>
2025-02-12 15:32:57 +0000

    Update app.py and requirements.txt with necessary changes

--- app.py
+++ app.py
@@ -247,7 +247,7 @@ def records():
                            prev_page=prev_page,
                            date_filter=date_filter)
 
-@app.route("/doctor")
+@app.route("/doctor-sugesstions")
 def doctor():
     return render_template('doctor-suggestion.html')
 
